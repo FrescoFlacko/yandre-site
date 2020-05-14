87f52e40c9d80fea88caf47bbf81fd625ccbe161
Yan <yanique.andre@gmail.com>
2020-05-13 21:39:24 -0400

    Finished Medium integration

--- src/app/molecule/medium-post-card/medium-post-card.component.ts
+++ src/app/molecule/medium-post-card/medium-post-card.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'app-medium-post-card',
   templateUrl: './medium-post-card.component.html',
+  // host: { 'class': 'row align-items-center' },
   styleUrls: ['./medium-post-card.component.scss']
 })
 export class MediumPostCardComponent implements OnInit {
+  @Input() imageUrl: string;
+  @Input() url: string;
+  @Input() title: string;
+  @Input() date: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.date = new Date(this.date).toDateString();
   }
-
 }
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, AfterViewChecked } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import * as Parser from 'rss-parser/dist/rss-parser.min.js';
+
+declare var test: any;
 
 @Component({
   selector: 'app-home-page',
@@ -8,13 +10,32 @@ import * as Parser from 'rss-parser/dist/rss-parser.min.js';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-  
+
+  public mediumPosts: Array<Object>;
+
+  private checked: boolean;
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     const parser = new Parser();
     parser.parseURL('https://medium.com/feed/@yan.dre').then(rss => {
       console.log(rss);
+      this.mediumPosts = rss.items.slice(0, 5);
+
+      for (const post of this.mediumPosts) {
+        const element = document.createElement('div');
+        element.innerHTML = post['content:encoded'];
+
+        post['imageSrc'] = element.querySelector('img').getAttribute('src');
+
+        if (!post['imageSrc'].includes('cdn-images')) {
+          post['imageSrc'] = 'assets/images/image-placeholder.jpg';
+        }
+      }
+    }).catch(() => {
+    }).finally(() => {
+      test();
     });
   }
 
